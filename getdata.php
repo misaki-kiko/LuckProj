cd48ecc03bcb4299c3dd089c09e6ff4b07da6a37
misaki-kiko <misaki@futa.in>
2018-10-12 19:45:12 +0800

    qwq

--- getdata.php
+++ getdata.php
@@ -1,19 +1,19 @@
 <?php
 $q = $_GET["q"];
 $servername = 'localhost';
-$username = 'testt';
-$password ='2333';
-$dbname = 'UserList';
+$username = 'qwq';
+$password ='qwq2333';
+$dbname = 'qwq';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
+echo "connected";
 mysql_select_db("User", $conn);
 
-$sql = "SELECT * FROM `User` WHERE `id` LIKE .$q.";
+$sql = "SELECT * FROM `User` WHERE `id` LIKE “‘”.$q.“‘”";
 
 $result = $conn->query($sql);
 
